35ab1fb6e57dc2ea22abb5374cdbc4327dda7c0f
ShineYu <shine.yulingle@gmail.com>
2023-02-11 19:59:20 +0800

    feat: 添加音乐界面。

--- source/js/anzhiyu.js
+++ source/js/anzhiyu.js
@@ -54,7 +54,59 @@ var anzhiyu = {
     }
     return arr[0];
   },
+  // 音乐节目切换背景
+  changeMusicBg: function (isChangeBg = true) {
+    if (window.location.pathname != "/music/") {
+      return;
+    }
+    const anMusicBg = document.getElementById("an_music_bg")
+
+    if (isChangeBg) {
+      // player listswitch 会进入此处
+      const musiccover = document.querySelector("#anMusic-page .aplayer-pic");
+      anMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+    } else {
+      // 第一次进入，绑定事件，改背景
+      let timer = setInterval(()=>{
+        const musiccover = document.querySelector("#anMusic-page .aplayer-pic");
+        // 确保player加载完成
+        console.info(anMusicBg);
+        if (musiccover) {
+          clearInterval(timer)
+          anMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+          // 绑定事件
+          anzhiyu.addEventListenerChangeMusicBg();
+          
+          // 暂停nav的音乐
+          if (document.querySelector("#nav-music meting-js").aplayer && !document.querySelector("#nav-music meting-js").aplayer.audio.paused){
+            anzhiyu.musicToggle()
+          }
+        }
+      }, 100)
+    }
+  },
+  addEventListenerChangeMusicBg: function () {
+    const anMusicPage = document.getElementById("anMusic-page");
+    const aplayerIconMenu = anMusicPage.querySelector(".aplayer-info .aplayer-time .aplayer-icon-menu");
+
+    anMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
+      anzhiyu.changeMusicBg();
+      console.info('player loadeddata');
+    });
+
+    aplayerIconMenu.addEventListener("click", function () {
+      document.getElementById('menu-mask').style.display = "block";
+      document.getElementById('menu-mask').style.animation = "0.5s ease 0s 1 normal none running to_show";
+    })
+
+    document.getElementById('menu-mask').addEventListener("click", function () {
+      if (window.location.pathname != "/music/") return;
+      anMusicPage.querySelector('.aplayer-list').classList.remove("aplayer-list-hide");
+    })
+  },
 };
 
 // 如果有右键事件 可以在这里写。
 // addRightMenuClickEvent();
+
+anzhiyu.changeMusicBg(false);  //音乐页面
