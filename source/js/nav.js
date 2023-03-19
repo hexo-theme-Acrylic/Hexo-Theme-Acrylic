30e46d97187a360ba303db2baeefebc2889e2479
timigomen <121149519+timigomen@users.noreply.github.com>
2023-03-19 12:21:25 +0800

    Update nav.js

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,6 +1,7 @@
-function whenDOMReady() {
-  "" === GLOBAL_CONFIG_SITE.title.replace("ZHHEO", "") ? document.getElementById("page-name-text").style.display = "none" : document.querySelector("#page-name-text>span").innerHTML = document.title.split(" | ZHHEO")[0];
+function navTitle() {
+    let e = document.title;
+    document.getElementById("page-name-text").innerHTML = e.replace(" | JayHrn", "")
 }
 
-whenDOMReady() // 打开网站先执行一次
-document.addEventListener("pjax:complete", whenDOMReady) // pjax加载完成（切换页面）后再执行一次
+navTitle() // 打开网站先执行一次
+document.addEventListener("pjax:complete", navTitle) // pjax加载完成（切换页面）后再执行一次
