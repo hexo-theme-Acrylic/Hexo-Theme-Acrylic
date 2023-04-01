60a20b81ec659615a3be25fde425865893224509
Marcus <marcus-zh@qq.com>
2023-04-01 16:34:43 +0800

    超多更改

--- scripts/helpers/page.js
+++ scripts/helpers/page.js
@@ -3,6 +3,7 @@
  * @example
  *  page_description()
  *  cloudTags(source, minfontsize, maxfontsize, limit)
+ *  pagecloud(source)
  */
 
 'use strict'
@@ -25,16 +26,7 @@ hexo.extend.helper.register('page_description', function () {
 hexo.extend.helper.register('cloudTags', function (options = {}) {
   const env = this
   let source = options.source
-  const minfontsize = options.minfontsize
-  const maxfontsize = options.maxfontsize
-  const limit = options.limit
-  const unit = options.unit || 'px'
-
   let result = ''
-  if (limit > 0) {
-    source = source.limit(limit)
-  }
-
   const sizes = []
   source.sort('length').forEach(tag => {
     const { length } = tag
@@ -42,13 +34,25 @@ hexo.extend.helper.register('cloudTags', function (options = {}) {
     sizes.push(length)
   })
 
-  const length = sizes.length - 1
   source.forEach(tag => {
-    const ratio = length ? sizes.indexOf(tag.length) / length : 0
-    const size = minfontsize + ((maxfontsize - minfontsize) * ratio)
-    let style = `font-size: ${parseFloat(size.toFixed(2))}${unit};`
-    const color = 'rgb(' + Math.floor(Math.random() * 201) + ', ' + Math.floor(Math.random() * 201) + ', ' + Math.floor(Math.random() * 201) + ')' // 0,0,0 -> 200,200,200
-    result += `<a href="${env.url_for(tag.path)}" style="${style}">${tag.name}<sup>${tag.length}</sup></a>`
+    result += `<a href="${env.url_for(tag.path)}">${tag.name}<sup>${tag.length}</sup></a>`
+  })
+  return result
+})
+
+hexo.extend.helper.register('pagecloud', function (options = {}) {
+  const env = this
+  let source = options.source
+  let result = ''
+  const sizes = []
+  source.sort('length').forEach(tag => {
+    const { length } = tag
+    if (sizes.includes(length)) return
+    sizes.push(length)
+  })
+
+  source.forEach(tag => {
+    result += `<a href="${env.url_for(tag.path)}" id="${tag.name}" data-pjax-state=""><span class="tags-punctuation"></span> ${tag.name} <span class="tagsPageCount">${tag.length}</span></a>`
   })
   return result
 })
