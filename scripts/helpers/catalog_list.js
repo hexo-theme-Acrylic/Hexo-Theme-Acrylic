62943e0fb69bd269c1f454b0f01e10c5739aad2e
Marcus <marcus-zh@qq.com>
2023-06-13 13:41:02 +0800

    add:很多页面,支持memos,页脚banner,最新文章.fix:分类,标签,归档子页面

--- scripts/helpers/catalog_list.js
+++ scripts/helpers/catalog_list.js
@@ -2,8 +2,8 @@ hexo.extend.helper.register('catalog_list', function (type) {
   let html = ``
   hexo.locals.get(type).map(function (item) {
     html += `
-    <div class="catalog-list-item" id="/${item.path}">
-      <a href="/${item.path}">${(hexo.config.emoji && hexo.config.emoji[item.name] || '') + item.name}<sup>${item.length}</sup></a>
+    <div class="category-bar-item" id="${item.name}">
+      <a href="/${item.path}">${item.name}</a>
     </div>
     `
   })
--- scripts/helpers/footer_friends_link.js
+++ scripts/helpers/footer_friends_link.js
@@ -5,7 +5,7 @@ hexo.extend.filter.register('after_generate', function () {
 
     let ls = [],
         data = YML.parse(fs.readFileSync('source/_data/link.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n'));
-
+    // console.log(data.link_list)
     data.forEach((e, i) => { if (i > 1) ls = ls.concat(e.link_list) });
     fs.writeFileSync('source/link.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`)
 }
